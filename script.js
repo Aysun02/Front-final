4427ec4bc9ea90c14dfa416314222d5cb0ae8a30
Aysun02 <104448452+Aysun02@users.noreply.github.com>
2022-09-11 20:44:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#checkbox')
+const body = document.body
+
+function change(){
+  btn.checked ? body.classList.add("dark") : body.classList.remove("dark")
+}
+
+
+
